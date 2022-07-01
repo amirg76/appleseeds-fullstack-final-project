6cedb945381e5169cfb9fa73713a52ba43267807
amir gilboa <amirg76@gmail.com>
2022-07-01 12:26:01 +0300

    first basic deploy

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">FIRST CONNECT</div>;
+  return <div className="App">SECOND CONNECT</div>;
 }
 
 export default App;
